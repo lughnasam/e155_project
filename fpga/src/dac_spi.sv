7f90366c99effbff023008f6fe0c83f804f7fd4c
Lughnasa Miller <lumiller@hmc.edu>
2025-11-15 17:00:26 -0800

    Started SPI Comms for FPGA controller

---
+++ fpga/src/dac_spi.sv
@@ -0,0 +1,11 @@
+// Names: Kanoa Parker and Lughnasa Miller
+// Date: 11/15/2025
+// Email: kanparker@hmc.edu, lumiller@hmc.edu
+// Description: This module controls SPI communication with an
+//              external MCP3202 ADC.
+
+module dac_spi(input logic  clk, cipo,
+               output logic copi, cs,
+               input logic [])
+
+endmodule
